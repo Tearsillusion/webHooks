63e0a35558e92e88805aa0f83df5229e6ff39922
lvjunling <1585345738@qq.com>
2022-05-10 15:27:30 +0800

    fix:修改index

--- mysql.sh
+++ mysql.sh
@@ -5,5 +5,5 @@ cd $WORK_PATH
 docker stop mysql-container
 docker rm mysql-container
 docker build -t mysql-images ./docker
-docker run -itd --name mysql-container -p 9004:9004 -e MYSQL_ROOT_PASSWORD=ljl1234 mysql-images
+docker run -itd --name mysql-container -p 9004:3306 -e MYSQL_ROOT_PASSWORD=ljl1234 mysql-images
 
